545dee58083690785f97504afab20964b25a42e1
infomanemasala-cmd <infomanemasala@gmail.com>
2026-09-15 12:46:06 +0530

    Use consolidated operations console for Payments

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,2 +1,2 @@
-import OperationsConsoleFinal from '@/components/operations-console-final'
-export default function PaymentsPage(){return <OperationsConsoleFinal module="payments"/>}
+import OperationsConsoleFinal2 from '@/components/operations-console-final2'
+export default function PaymentsPage(){return <OperationsConsoleFinal2 module="payments"/>}
