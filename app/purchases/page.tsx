aaf6ab6a1e34afeee25f98005ac7c716c5d10352
Mane Masala <infomanemasala@gmail.com>
2026-09-16 18:55:19 +0530

    feat: use simplified purchase workflow

--- app/purchases/page.tsx
+++ app/purchases/page.tsx
@@ -1,5 +1,5 @@
-import PurchaseConsoleV2 from '@/components/purchase-console-v2'
+import PurchaseConsoleV3 from '@/components/purchase-console-v3'
 
 export default function PurchasesPage() {
-  return <PurchaseConsoleV2 />
+  return <PurchaseConsoleV3 />
 }
