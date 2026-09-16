c67bb455cd9041028a4e3fd064d53c7882092719
infomanemasala-cmd <infomanemasala@gmail.com>
2026-09-16 17:19:48 +0530

    style: load final branding and order workflow styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import './globals.css'
 import './phase4e.css'
 import './phase4f.css'
+import './phase4g.css'
 import ThemePicker from '@/components/theme-picker'
 
 export const metadata: Metadata = { title:'Mane Masala Business System', description:'Business management system for Mane Masala' }
