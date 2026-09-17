3b6a7d60cea6504f4d6700ef9e404dbbd88d0e4c
Mane Masala <infomanemasala@gmail.com>
2026-09-17 11:24:26 +0530

    fix: expose purchase history in standardized table

--- app/purchases/page.tsx
+++ app/purchases/page.tsx
@@ -1,5 +1,11 @@
 import PurchaseConsoleV3 from '@/components/purchase-console-v3'
+import PurchaseHistoryPanel from '@/components/purchase-history-panel'
 
 export default function PurchasesPage() {
-  return <PurchaseConsoleV3 />
+  return <>
+    <PurchaseConsoleV3 />
+    <section className="page-panel" style={{ paddingTop: 0 }}>
+      <PurchaseHistoryPanel />
+    </section>
+  </>
 }
